206e67b121873ed87460c617f5d11fa31f4e5126
larissa-dias3 <154644799+larissa-dias3@users.noreply.github.com>
2024-12-03 05:54:00 +0000

    mudando

--- scripts/lanches.js
+++ scripts/lanches.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function buscarLanches(){
-    fetch('http://localhost:3000/bebidas/')
+    fetch('http://localhost:3000/lanches/')
     .then(function(response) {
         return response.json();
     })
@@ -32,11 +32,37 @@ function buscarLanches(){
 }
 
 function criarCardapio(lanches) {
-    const card = document.createElement('div');
-    card.classList.add('card');
+    const section = document.querySelector('section');
 
-    const img = document.createElement('img');
-    let imgSrc = bebidas.geladas[0].preco;
-    console.log(imgSrc);
-    img.src = imgSrc;
+    lanches.forEach(lanche => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const infos = document.createElement('div');
+        infos.classList.add('informacoes');
+        const produto = document.createElement('div');
+        produto.classList.add('produto');
+        const preco = document.createElement('div');
+        preco.classList.add('preco');
+
+        const img = document.createElement('img');
+        img.src = `../assets/${lanche.imagem}`;
+        const h2 = document.createElement('h2');
+        h2.textContent = lanche.nome;
+        const pProduto = document.createElement('p');
+        pProduto.textContent = lanche.descricao;
+        const pPreco = document.createElement('p');
+        pPreco.textContent = lanche.preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});;
+        const svgImg = document.createElement('img');
+        svgImg.src = '../assets/carrinho.svg';
+
+        preco.appendChild(pPreco);
+        preco.appendChild(svgImg);
+        produto.appendChild(h2);
+        produto.appendChild(pProduto);
+        infos.appendChild(produto);
+        infos.appendChild(preco);
+        card.appendChild(img);
+        card.appendChild(infos);
+        section.appendChild(card);
+    });
 }
