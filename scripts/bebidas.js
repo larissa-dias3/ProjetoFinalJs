da5926e2364e8a5e9f99164a7f7aeb8e78223404
Marco Sieben <57383943+sieben-marco@users.noreply.github.com>
2024-12-03 03:21:52 +0000

    fetch do cardapio

--- scripts/bebidas.js
+++ scripts/bebidas.js
@@ -1,27 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let dados = buscarBebidas();
-    console.log(dados);
+    buscarBebidas();
 });
 
 function buscarBebidas(){
-    fetch('http://localhost:3000/')
-    .then(function(response){
-        let bebidas = response.json();
-        console.log(bebidas);
-        return bebidas;
+    fetch('http://localhost:3000/bebidas/')
+    .then(function(response) {
+        return response.json();
     })
-    // .then(function(bebidas){
-    //     var lista = document.getElementById('lista-bebidas');
-    //     lista.innerHTML = '';
-    //     for(var i = 0; i < bebidas.length; i++){
-    //         var li = document.createElement('li');
-    //         li.innerHTML = bebidas[i].nome + ' - R$ ' + bebidas[i].preco;
-    //         lista.appendChild(li);
-    //     }
-    // })
-    .catch(function(erro){
+    .then(function(bebidas) {
+        criarCardapio(bebidas);
+    })
+    .catch(function(erro) {
         console.error('Erro ao buscar bebidas:', erro);
     });
 }
 
+function criarCardapio(bebidas) {
+    const card = document.createElement('div');
+    card.classList.add('card');
 
+    const img = document.createElement('img');
+    let imgSrc = bebidas.geladas[0].preco;
+    console.log(imgSrc);
+    img.src = imgSrc;
+
+
+    // let lista = document.getElementById('lista-bebidas');
+    // lista.innerHTML = '';
+    // for(let i = 0; i < bebidas.length; i++){
+    //     let li = document.createElement('li');
+    //     li.innerHTML = bebidas[i].nome + ' - R$ ' + bebidas[i].preco;
+    //     lista.appendChild(li);
+    // }
+}
